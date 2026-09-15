40ebb392a0190eda0558a72944157ff05bea9ff4
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-09-15 13:21:25 +0300

    Create messenger.hh

---
+++ GEANT4/Optimization_Loop/Comparative_Loop/messenger.hh
@@ -0,0 +1,30 @@
+#ifndef MESSENGER_HH
+#define MESSENGER_HH
+
+#include "G4UImessenger.hh"
+#include "G4UIcmdWithADoubleAndUnit.hh"
+
+class MyDetectorConstruction;
+
+class MyDetectorMessenger : public G4UImessenger
+{
+public:
+    MyDetectorMessenger(MyDetectorConstruction* myDet);
+    ~MyDetectorMessenger();
+    
+    virtual void SetNewValue(G4UIcommand* command, G4String newValue) override;
+
+private:
+    MyDetectorConstruction* fDetectorConstruction;
+    
+    G4UIcmdWithADoubleAndUnit* fModThicknessCmd;
+    G4UIcmdWithADoubleAndUnit* fReflectorThicknessCmd;
+    G4UIcmdWithADoubleAndUnit* fFastFilterThicknessCmd;
+    G4UIcmdWithADoubleAndUnit* fGammaFilterThicknessCmd;
+    G4UIcmdWithADoubleAndUnit* fCollimatorLengthCmd;
+    
+};
+
+#endif
+   
+   
